93a7d44389d68e0b2e3a2e33460d9a67cfe1626d
ElDainosor <eldainosor@gmail.com>
2018-02-09 18:58:38 +0000

    Lineageify

--- lineage.mk
+++ lineage.mk
@@ -17,14 +17,10 @@ $(call inherit-product, device/motorola/surnia/full_surnia.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := surnia
-PRODUCT_NAME := validus_surnia
+PRODUCT_NAME := lineage_surnia
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := surnia
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="Moto E LTE (2nd Gen)"
-
-# Validus Device Maintainers
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    DEVICE_MAINTAINERS="Sean Pires (seAndroid)"

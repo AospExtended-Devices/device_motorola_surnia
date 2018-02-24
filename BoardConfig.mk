f2c7d7be0a0d93fe0e514a884ae694eaeea710dc
ElDainosor <eldainosor@gmail.com>
2018-02-24 01:02:56 +0000

    surnia: Bootleggers Bringup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,8 +24,7 @@ DEVICE_PATH := device/motorola/surnia
 TARGET_KERNEL_CONFIG := surnia_defconfig
 
 # Kernel Toolchain
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9/bin
-KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+KERNEL_TOOLCHAIN_PREFIX := arm-linux-androidkernel-
 
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := XT1514,XT1521,XT1524,XT1526,XT1527,XT1523,surnia_uds,surnia_umts,surnia,surnia_udstv
--- bootleg.mk
+++ bootleg.mk
@@ -17,10 +17,14 @@ $(call inherit-product, device/motorola/surnia/full_surnia.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := surnia
-PRODUCT_NAME := lineage_surnia
+PRODUCT_NAME := bootleg_surnia
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := surnia
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="Moto E LTE (2nd Gen)"
+
+# Validus Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="ElDainosor"
